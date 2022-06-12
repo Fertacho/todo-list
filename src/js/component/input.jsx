d1073294f8817c4d66619d6af80b4611dc4bc498
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-06-12 23:15:34 +0000

    avance

---
+++ src/js/component/input.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Input = () => {
+	return <div className="contenedor">input</div>;
+};
+
+export default Input;
---
+++ src/js/component/item.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Item = () => {
+	return <div className="contenedor">item</div>;
+};
+
+export default Item;
