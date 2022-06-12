ff5f4031670ca75edef22e6ba7b323712b865dc6
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-06-12 23:58:57 +0000

    avance

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Input = () => {
-	return <div className="contenedor">input</div>;
+	return (
+		<div className="contenedor">
+			<input className="listwrite" placeholder="Agregar tarea"></input>
+		</div>
+	);
 };
 
 export default Input;
--- src/js/component/item.jsx
+++ src/js/component/item.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Item = () => {
-	return <div className="contenedor">item</div>;
+	return <div className="contenedor">adda</div>;
 };
 
 export default Item;
