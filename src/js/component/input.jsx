b6e516be07ac0218a645a537365140828981c60b
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-06-13 19:57:33 +0000

    avance

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -4,7 +4,7 @@ const Input = () => {
 	return (
 		<div className="contenedor">
 			<input id="myInput" placeholder="Agregar tarea"></input>
-			<button onclick="Item()" class="addBtn">
+			<button onclick="Item()" class="myBtn">
 				agregar
 			</button>
 		</div>
